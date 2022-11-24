1b1c17344b6440d684fa3722387f193410b8269c
Jorge Vásquez <jorge.vasquez@utec.edu.pe>
2022-11-23 21:17:15 -0500

    feat(client) login and register video recorder

--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -3,12 +3,14 @@ import { LockClosedIcon } from '@heroicons/react/20/solid'
 import { useState } from 'react'
 import { signIn } from 'next-auth/react'
 import { useRouter } from 'next/router'
+import VideoRecorder from 'react-video-recorder'
+import "react-toastify/dist/ReactToastify.css";
+
 
 
 export default function SingIn() {
 
-    const [userInfo, setUserInfo] = useState({ email: "", password: "" })
-    const [error, setError] = useState({ ok: true, error: "" })
+    const [userInfo, setUserInfo] = useState({ email: "", password: "", video: null })
     const router = useRouter()
 
     const handleSubmit = async (e) => {
@@ -17,42 +19,31 @@ export default function SingIn() {
         const res = await signIn('credentials', {
             email: userInfo.email,
             password: userInfo.password,
+            any_video : userInfo.video,
             redirect: false
         })
         // console.log(res)
-        setError({ ok: res.ok, error: res.error })
         if (res.status == 200) {
             router.push('/')
         }
     }
 
-    const Notficitation = () => {
-        if (!error.ok) {
-            return (
-                <div className="px-8 py-6 bg-red-400 text-white flex justify-between rounded absolute top-3 right-3">
-                    <div className="flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 mr-6" viewBox="0 0 20 20" fill="currentColor">
-                            <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
-                        </svg>
-                        <p>{error.error}!</p>
-                    </div>
-                    <button className="text-red-100 hover:text-white" onClick={() => setError({ ok: true, error: "" })}>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round"strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-                        </svg>
-                    </button>
-                </div>
-            )
-        }
-        return (<></>)
+    const onComplete = (videoBlob) => {
+        console.log("videoBlob", videoBlob)
+        // const urlObject = window.URL.createObjectURL(videoBlob);
+        // const link = document.createElement('a');
+        // link.href = urlObject;
+        // link.setAttribute('download', 'recording2');
+        // document.body.appendChild(link);
+        // link.click();
+        // document.body.removeChild(link);
+        setRegister({ ...userInfo, video: videoBlob })
     }
-
 
 
     return (
         <>
-            {Notficitation()}
-            <section className="bg-gray-50 dark:bg-gray-900">
+            <main className="bg-gray-50 dark:bg-gray-900">
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -65,7 +56,7 @@ export default function SingIn() {
                                     <input type="email"
                                         value={userInfo.email}
                                         onChange={(e) => setUserInfo({ ...userInfo, email: e.target.value })}
-                                        name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" 
+                                        name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com"
                                         required />
                                 </div>
                                 <div>
@@ -74,8 +65,18 @@ export default function SingIn() {
                                         value={userInfo.password}
                                         onChange={(e) => setUserInfo({ ...userInfo, password: e.target.value })}
                                         name="password" id="password" placeholder="••••••••"
-                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                         required />
+                                </div>
+
+                                <div>
+                                    <VideoRecorder
+                                        isOnInitially={true}
+                                        onRecordingComplete={(videoBlob) => onComplete(videoBlob)}
+                                        isFlipped={true}
+                                        countdownTime={3000}
+                                        timeLimit={5000}
+                                    />
                                 </div>
 
                                 <button
@@ -87,7 +88,6 @@ export default function SingIn() {
                                     </span>
                                     Sign in
                                 </button>
-                                {/* <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button> */}
                                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                                     Don’t have an account yet? <a onClick={() => router.push('/auth/signup')} className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
                                 </p>
@@ -95,7 +95,7 @@ export default function SingIn() {
                         </div>
                     </div>
                 </div>
-            </section>
+            </main>
         </>
     )
 }
--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -2,6 +2,7 @@
 import { useRouter } from "next/router"
 import { ArrowRightOnRectangleIcon } from '@heroicons/react/20/solid'
 import { useState } from "react";
+import VideoRecorder from 'react-video-recorder'
 
 export default function SingUp() {
     const router = useRouter();
@@ -12,7 +13,7 @@ export default function SingUp() {
             userName: "",
             email: "",
             password: "",
-            videoPath: ""
+            first_video: null,
         }
     );
 
@@ -22,9 +23,20 @@ export default function SingUp() {
         console.log(register)
         // router.push('/auth/signin')
     }
+    const onComplete = (videoBlob) => {
+        console.log("videoBlob", videoBlob)
+        // const urlObject = window.URL.createObjectURL(videoBlob);
+        // const link = document.createElement('a');
+        // link.href = urlObject;
+        // link.setAttribute('download', 'recording2');
+        // document.body.appendChild(link);
+        // link.click();
+        // document.body.removeChild(link);
+        setRegister({ ...register, first_video: videoBlob })
+    }
 
     return (<>
-        <section className="bg-gray-50 dark:bg-gray-900">
+        <main className="bg-gray-50 dark:bg-gray-900">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
 
                 <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
@@ -34,26 +46,30 @@ export default function SingUp() {
                         </h1>
                         <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
 
-                            <div>
-                                <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
-                                <input type="text"
-                                    name="first_name"
-                                    id="first_name"
-                                    value={register.firstName}
-                                    onChange={(e) => setRegister({ ...register, firstName: e.target.value })}
-                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    required />
+                            <div className="flex space-x-2">
+                                <div >
+                                    <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
+                                    <input type="text"
+                                        name="first_name"
+                                        id="first_name"
+                                        value={register.firstName}
+                                        onChange={(e) => setRegister({ ...register, firstName: e.target.value })}
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        required />
+                                </div>
+                                <div >
+                                    <label htmlFor="last_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
+                                    <input type="text"
+                                        name="last_name"
+                                        id="last_name"
+                                        value={register.lastName}
+                                        onChange={(e) => setRegister({ ...register, lastName: e.target.value })}
+                                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        required />
+                                </div>
+
                             </div>
-                            <div>
-                                <label htmlFor="last_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
-                                <input type="text"
-                                    name="last_name"
-                                    id="last_name"
-                                    value={register.lastName}
-                                    onChange={(e) => setRegister({ ...register, lastName: e.target.value })}
-                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    required />
-                            </div>
+
                             <div>
                                 <label htmlFor="user_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">User Name</label>
                                 <input type="text"
@@ -77,14 +93,24 @@ export default function SingUp() {
                             </div>
                             <div>
                                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input type="password" 
-                                name="password" 
-                                id="password" 
-                                placeholder="••••••••" 
-                                value={register.password}
-                                onChange={(e) => setRegister({ ...register, password: e.target.value })}
-                                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                                required />
+                                <input type="password"
+                                    name="password"
+                                    id="password"
+                                    placeholder="••••••••"
+                                    value={register.password}
+                                    onChange={(e) => setRegister({ ...register, password: e.target.value })}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    required />
+                            </div>
+
+                            <div>
+                                <VideoRecorder
+                                    isOnInitially={true}
+                                    onRecordingComplete={(videoBlob) => onComplete(videoBlob)}
+                                    isFlipped={true}
+                                    countdownTime={3000}
+                                    timeLimit={5000}
+                                />
                             </div>
 
                             <button
@@ -103,7 +129,7 @@ export default function SingUp() {
                     </div>
                 </div>
             </div>
-        </section>
+        </main>
 
     </>)
 
