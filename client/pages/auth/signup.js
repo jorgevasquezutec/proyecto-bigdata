4c1f715a96e4d925bfdfe8430b0f45955fcfbc22
Jorge Vásquez <jorge.vasquez@utec.edu.pe>
2022-11-23 19:30:52 -0500

    fix(client) validation form

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,32 +1,92 @@
 
 import { useRouter } from "next/router"
 import { ArrowRightOnRectangleIcon } from '@heroicons/react/20/solid'
+import { useState } from "react";
 
 export default function SingUp() {
-    const router = useRouter()
+    const router = useRouter();
+    const [register, setRegister] = useState(
+        {
+            firstName: "",
+            lastName: "",
+            userName: "",
+            email: "",
+            password: "",
+            videoPath: ""
+        }
+    );
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("entro")
+        console.log(register)
+        // router.push('/auth/signin')
+    }
 
     return (<>
-        <section class="bg-gray-50 dark:bg-gray-900">
-            <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-               
-                <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                    <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+        <section className="bg-gray-50 dark:bg-gray-900">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Create and account
                         </h1>
-                        <form class="space-y-4 md:space-y-6" action="#">
+                        <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+
                             <div>
-                                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required=""/>
+                                <label htmlFor="first_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
+                                <input type="text"
+                                    name="first_name"
+                                    id="first_name"
+                                    value={register.firstName}
+                                    onChange={(e) => setRegister({ ...register, firstName: e.target.value })}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    required />
                             </div>
                             <div>
-                                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                                <label htmlFor="last_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
+                                <input type="text"
+                                    name="last_name"
+                                    id="last_name"
+                                    value={register.lastName}
+                                    onChange={(e) => setRegister({ ...register, lastName: e.target.value })}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    required />
                             </div>
                             <div>
-                                <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                                <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                                <label htmlFor="user_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">User Name</label>
+                                <input type="text"
+                                    name="user_name"
+                                    id="user_name"
+                                    value={register.userName}
+                                    onChange={(e) => setRegister({ ...register, userName: e.target.value })}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    required />
                             </div>
+                            <div>
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                <input type="email"
+                                    name="email"
+                                    id="email"
+                                    value={register.email}
+                                    onChange={(e) => setRegister({ ...register, email: e.target.value })}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="name@company.com"
+                                    required />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                <input type="password" 
+                                name="password" 
+                                id="password" 
+                                placeholder="••••••••" 
+                                value={register.password}
+                                onChange={(e) => setRegister({ ...register, password: e.target.value })}
+                                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                                required />
+                            </div>
+
                             <button
                                 onClick={() => router.push('/auth/signup')}
                                 className="group relative flex w-full justify-center rounded-md border border-transparent bg-green-600 py-2 px-4 text-sm font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
@@ -36,8 +96,8 @@ export default function SingUp() {
                                 </span>
                                 Sign up
                             </button>
-                            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Already have an account? <a onClick={()=>router.push('/auth/signin')} class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign In here</a>
+                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                Already have an account? <a onClick={() => router.push('/auth/signin')} className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign In here</a>
                             </p>
                         </form>
                     </div>
