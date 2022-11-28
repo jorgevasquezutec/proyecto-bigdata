983e3a654bdf947787b6fdbc50edf480f8e3c121
joseporres <jose.porres@utec.edu.pe>
2022-11-28 13:54:31 -0500

    feat: fake detector modification

--- kafka-server/src/FakeDetector.py
+++ kafka-server/src/FakeDetector.py
@@ -2,9 +2,38 @@ from confluent_kafka import Consumer, Producer
 import json
 import os
 import socket
+import torchvision
+import torch.nn as nn
+import torch
+import numpy as np
+import cv2
 from dotenv import load_dotenv
+from typing import List, Dict, Union
+IMG_SIZE: int = 224
+NUM_FRAMES_PER_VIDEO: int = 16
+
 load_dotenv()
 
+def video2frames(video_path: str, resize: Union[int, int] = (IMG_SIZE, IMG_SIZE)) -> np.array:
+    cap = cv2.VideoCapture(video_path)
+    frames: list = []
+    is_there_frame: bool = True
+    num_total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    resampling_rate: int = int(num_total_frames / NUM_FRAMES_PER_VIDEO)
+    idf: int = 0
+    while is_there_frame and len(frames) < NUM_FRAMES_PER_VIDEO:
+        idf += 1
+        is_there_frame, frame = cap.read()
+        if frame is None: 
+            return np.array([])
+        if idf % resampling_rate == 0:
+            # grayscale
+            # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # resize
+            frame = cv2.resize(frame, resize)
+            frames.append(frame)
+    assert len(frames)==NUM_FRAMES_PER_VIDEO
+    return np.array(frames)
 
 class FakeDetector:
     def __init__(self):
@@ -28,8 +57,17 @@ class FakeDetector:
 
     def filtered(self, msg):
         event = json.loads(msg.decode('utf-8'))
-        print(event)
-        return True
+        any_video = event['any_video']
+        model = torchvision.models.resnet18(pretrained=True)
+        for e in model.parameters():
+            e.requires_grad = False
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        model.fc = nn.Linear(in_features=512, out_features=4, bias=True)
+        model.load_state_dict(torch.load("../models/resnet18_5.pt"))
+        model.to(device)
+        outputs = model(video2frames(any_video))
+        _, predicted = torch.max(outputs.data, 1)
+        return predicted == 0
 
     def consume(self, topic):
         self.consumer = Consumer(self.confConsumer)
