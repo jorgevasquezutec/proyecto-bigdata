fcaa8df4bfa5db7ad27a5fdefe98c44433957abb
jorgevasquezutec <jorge.vasquez@utec.edu.pe>
2023-07-15 02:17:47 -0500

    feat:finish all

--- client/services/api.js
+++ client/services/api.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
-import getConfig from "next/config";
+// import getConfig from "next/config";
 
 const header = {
     headers : {
         'Content-Type': 'application/json',
     }
 }
-const API_URL = getConfig().publicRuntimeConfig.API_URL;
+// const API_URL = getConfig().publicRuntimeConfig.API_URL;
 
 
 export const ApiRegister = async (paylaod) => {
     return new Promise(async(resolve, reject) => {
         try {
-            const res = await axios.post(`${API_URL}/auth/signup`, paylaod)
+            const res = await axios.post(`/server/auth/signup`, paylaod)
             resolve(res)
         } catch (error) {
             reject(error)
@@ -23,7 +23,7 @@ export const ApiRegister = async (paylaod) => {
 export const ApiLogin = async (paylaod) => {
     return new Promise(async(resolve, reject) => {
         try {
-            const res = await axios.post(`${API_URL}/auth/login`, paylaod)
+            const res = await axios.post(`/server/auth/login`, paylaod)
             resolve(res)
         } catch (error) {
             reject(error)
